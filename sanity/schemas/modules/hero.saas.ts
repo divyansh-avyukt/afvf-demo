8ea04a7b9207cb46c551228573a3ea9bc713a6bc
Divyansh Gupta <divyansh@avyuktlabs.in>
2024-09-25 12:40:02 +0530

    Initial commit

---
+++ sanity/schemas/modules/hero.saas.ts
@@ -0,0 +1,86 @@
+import { defineField, defineType } from 'sanity'
+import { TfiLayoutCtaCenter } from 'react-icons/tfi'
+import { getBlockText } from '@sanity/src/utils'
+
+export default defineType({
+	name: 'hero.saas',
+	title: 'Hero (SaaS)',
+	icon: TfiLayoutCtaCenter,
+	type: 'object',
+	groups: [
+		{ name: 'content', default: true },
+		{ name: 'image' },
+		{ name: 'options' },
+	],
+	fields: [
+		defineField({
+			name: 'pretitle',
+			type: 'string',
+			group: 'content',
+		}),
+		defineField({
+			name: 'content',
+			type: 'array',
+			of: [{ type: 'block' }],
+			group: 'content',
+		}),
+		defineField({
+			name: 'ctas',
+			title: 'Call-to-actions',
+			type: 'array',
+			of: [{ type: 'cta' }],
+			group: 'content',
+		}),
+		defineField({
+			name: 'reputation',
+			type: 'reference',
+			to: [{ type: 'reputation' }],
+			group: 'content',
+		}),
+		defineField({
+			name: 'image',
+			type: 'image',
+			options: {
+				hotspot: true,
+			},
+			group: 'image',
+			fields: [
+				defineField({
+					name: 'alt',
+					type: 'string',
+				}),
+				defineField({
+					name: 'faded',
+					type: 'boolean',
+					initialValue: true,
+				}),
+				defineField({
+					name: 'loading',
+					type: 'string',
+					options: {
+						list: ['lazy', 'eager'],
+						layout: 'radio',
+					},
+					initialValue: 'lazy',
+				}),
+			],
+		}),
+		defineField({
+			name: 'uid',
+			title: 'Unique Identifier',
+			type: 'uid',
+			group: 'options',
+		}),
+	],
+	preview: {
+		select: {
+			content: 'content',
+			media: 'image',
+		},
+		prepare: ({ content, media }) => ({
+			title: getBlockText(content),
+			subtitle: 'Hero (SaaS)',
+			media,
+		}),
+	},
+})
---
+++ src/ui/modules/HeroSaaS.tsx
@@ -0,0 +1,46 @@
+import { PortableText } from 'next-sanity'
+import Pretitle from '@/ui/Pretitle'
+import Reputation from '@/ui/Reputation'
+import CTAList from '@/ui/CTAList'
+import Img from '@/ui/Img'
+import { cn } from '@/lib/utils'
+import moduleProps from '@/lib/moduleProps'
+
+export default function HeroSaaS({
+	pretitle,
+	content,
+	ctas,
+	reputation,
+	image,
+	...props
+}: Partial<{
+	pretitle: string
+	content: any
+	ctas: Sanity.CTA[]
+	reputation: Sanity.Reputation
+	image: Sanity.Image & {
+		faded?: boolean
+	}
+}> &
+	Sanity.Module) {
+	return (
+		<section className="section space-y-8 text-center" {...moduleProps(props)}>
+			<div className="richtext mx-auto max-w-2xl text-balance">
+				<Pretitle>{pretitle}</Pretitle>
+				<PortableText value={content} />
+				<Reputation className="!mt-4 justify-center" reputation={reputation} />
+				<CTAList ctas={ctas} className="!mt-8 justify-center" />
+			</div>
+
+			<Img
+				image={image}
+				className={cn(
+					'anim-fade-to-t [animation-duration:1s]',
+					image?.faded &&
+						'[mask-image:linear-gradient(to_bottom,#000_50%,transparent)]',
+				)}
+				draggable={false}
+			/>
+		</section>
+	)
+}
